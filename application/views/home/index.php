fedb73afb47e2d6cedc4227e812bef9924882a56
Sindu Maulana <sindu.maulana@gmail.com>
2020-10-19 20:47:11 +0700

    mambuat ci3

---
+++ application/views/home/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hello, <?= $nama; ?> !</h1>
+      <p class="lead">Programmer pemula</p>
+    </div>
+  </div>
+</div>
